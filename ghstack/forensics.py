153693c2b3767a69a82faf39fca604b4a14f59aa
Josh Kuhn <94643789+josh-braid@users.noreply.github.com>
2023-12-05 11:41:53 -0500

    Refactoring

--- ghstack/forensics.py
+++ ghstack/forensics.py
@@ -1,69 +1,88 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# This script looks at all commits downloads their logs and prints them
-# for you
-
 import re
-from typing import Any, Dict
-
 import aiohttp
+from typing import Any, Dict
 
 import ghstack.circleci
 import ghstack.github
 import ghstack.github_utils
 
-RE_CIRCLECI_URL = re.compile(r"^https://circleci.com/gh/pytorch/pytorch/([0-9]+)")
+# Constants
+CIRCLECI_URL_PATTERN = r"^https://circleci.com/gh/pytorch/pytorch/([0-9]+)"
+SCCACHE_MARKER = "=================== sccache compilation log ==================="
+CI_CONTEXT = "ci/circleci: pytorch_linux_xenial_py3_clang5_asan_test"
+
+re_circleci_url = re.compile(CIRCLECI_URL_PATTERN)
 
 
-def strip_sccache(x: str) -> str:
-    sccache_marker = "=================== sccache compilation log ==================="
-    marker_pos = x.rfind(sccache_marker)
-    newline_before_marker_pos = x.rfind("\n", 0, marker_pos)
-    return x[:newline_before_marker_pos]
+def strip_sccache(log_content: str) -> str:
+    """
+    Strip sccache compilation log from the log content.
+    """
+    marker_pos = log_content.rfind(SCCACHE_MARKER)
+    newline_before_marker_pos = log_content.rfind("\n", 0, marker_pos)
+    return log_content[:newline_before_marker_pos]
 
 
-async def main(
-    pull_request: str,
-    github: ghstack.github.GitHubEndpoint,
-    circleci: ghstack.circleci.CircleCIEndpoint,
-) -> None:
+async def fetch_and_process_log(circleci: ghstack.circleci.CircleCIEndpoint, build_id: str, params: Dict[str, Any]) -> str:
+    """
+    Fetch and process the log for a given build ID.
+    """
+    try:
+        response = await circleci.get(f"project/github/{params['name']}/{params['owner']}/{build_id}")
+        if not response["failed"]:
+            return "❔"
+        async with aiohttp.request("GET", response["steps"][-1]["actions"][-1]["output_url"]) as resp:
+            log_json = await resp.json()
+            log_messages = [entry["message"] for entry in log_json]
+            return "\n" + strip_sccache("\n".join(log_messages))[-1500:]
+    except Exception as e:
+        return f"Error fetching log: {e}"
 
-    # Game plan:
-    # 1. Query GitHub to find out what the current statuses are
-    #       (TODO: if we got rate limited we'll miss stuff)
-    # 2. For each status in parallel:
-    #   a. Query CircleCI for job status
-    #   b. (Future work) Query output_url to get log information
-    #      (it's gzip'ed)
-    #
-    # For now:
-    #   - Print if the job actually ran, or was skipped
-    #       - Easy way to determine: check if "Should run job after
-    #         checkout" is last step
-    #       - I inspected circleci.get('project/github/pytorch/pytorch/1773555')
-    #         to see if there were other options, there did not appear
-    #         to be any indication that a halt was called.  So we'll
-    #         have to rely on the (OS X jobs, take note!)
 
+async def process_commit_node(node: Dict[str, Any], circleci: ghstack.circleci.CircleCIEndpoint, params: Dict[str, Any]) -> str:
+    """
+    Process a single commit node and return formatted output.
+    """
+    commit = node["commit"]
+    status = commit.get("status")
+    if not status:
+        return f"❔ {commit['oid'][:8]} {commit['messageHeadline']}"
+
+    for context in status["contexts"]:
+        if context["context"] != CI_CONTEXT:
+            continue
+        match = re_circleci_url.match(context["targetUrl"])
+        if not match:
+            return f"🍆 {commit['oid'][:8]} {commit['messageHeadline']}"
+
+        build_id = match.group(1)
+        log_text = await fetch_and_process_log(circleci, build_id, params)
+        state_icon = "✅" if context["state"] == "SUCCESS" else "❌"
+        return f"{state_icon} {commit['oid'][:8]} {commit['messageHeadline']} ({build_id}){log_text}"
+
+    return "❔ Unknown Status"
+
+
+async def main(pull_request: str, github: ghstack.github.GitHubEndpoint, circleci: ghstack.circleci.CircleCIEndpoint) -> None:
     params = ghstack.github_utils.parse_pull_request(pull_request)
-
-    # TODO: stop hard-coding number of commits
-    r = github.graphql(
-        """
-    query ($name: String!, $owner: String!, $number: Int!) {
-        repository(name: $name, owner: $owner) {
-            pullRequest(number: $number) {
-                commits(last: 100) {
-                    nodes {
-                        commit {
-                            oid
-                            messageHeadline
-                            status {
-                                contexts {
-                                    context
-                                    state
-                                    targetUrl
+    query = """
+        query ($name: String!, $owner: String!, $number: Int!) {
+            repository(name: $name, owner: $owner) {
+                pullRequest(number: $number) {
+                    commits(last: 100) {
+                        nodes {
+                            commit {
+                                oid
+                                messageHeadline
+                                status {
+                                    contexts {
+                                        context
+                                        state
+                                        targetUrl
+                                    }
                                 }
                             }
                         }
@@ -71,59 +90,18 @@ async def main(
                 }
             }
         }
-    }
-    """,
-        **params
-    )
-    nodes = r["data"]["repository"]["pullRequest"]["commits"]["nodes"]
+    """
+    try:
+        response = await github.graphql(query, **params)
+        nodes = response["data"]["repository"]["pullRequest"]["commits"]["nodes"]
+        results = await asyncio.gather(*(process_commit_node(n, circleci, params) for n in nodes))
+        for result in results:
+            print(result)
+    except Exception as e:
+        print(f"Error processing pull request: {e}")
 
-    async def process_node(n: Dict[str, Any]) -> str:
-        commit = n["commit"]
-        status = commit["status"]
-        icon = "❔"
-        text = ""
-        buildid_text = ""
-        if status is not None:
-            contexts = status["contexts"]
-        else:
-            contexts = []
-        for c in contexts:
-            # TODO: Stop hard-coding me
-            if c["context"] != "ci/circleci: pytorch_linux_xenial_py3_clang5_asan_test":
-                continue
-            m = RE_CIRCLECI_URL.match(c["targetUrl"])
-            if not m:
-                icon = "🍆"
-                break
-            if c["state"] == "SUCCESS":
-                icon = "✅"
-                break
-            buildid = m.group(1)
-            buildid_text = " ({})".format(buildid)
-            r = await circleci.get(
-                "project/github/{name}/{owner}/{buildid}".format(
-                    buildid=buildid, **params
-                )
-            )
-            if not r["failed"]:
-                # It was just cancelled (don't check "cancelled"; that's
-                # true even if the job failed otherwise; it just means
-                # workflow got cancelled)
-                icon = "❔"
-                break
-            icon = "❌"
-            async with aiohttp.request(
-                "get", r["steps"][-1]["actions"][-1]["output_url"]
-            ) as resp:
-                log_json = await resp.json()
-                buf = []
-                for e in log_json:
-                    buf.append(e["message"])
-                text = "\n" + strip_sccache("\n".join(buf))
-                text = text[-1500:]
-        return "{} {} {}{}{}".format(
-            icon, commit["oid"][:8], commit["messageHeadline"], buildid_text, text
-        )
 
-    for n in nodes:
-        print(await process_node(n))
+# The main entry point for the script
+if __name__ == "__main__":
+    # Initialize GitHub and CircleCI endpoints here and call main()
+    pass
